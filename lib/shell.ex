b0178aa85feca0d406171039d03fc9086fe1a3f2
Samuel Ventura <samuel.ventura@yeico.com>
2022-12-18 18:23:40 -0600

    Added canvas rgb to hex. Fixed eval not catching process exits. Rpi3 fb cleans up on exit.

--- lib/shell.ex
+++ lib/shell.ex
@@ -22,21 +22,72 @@ defmodule Ash.App.Shell do
     IEx.start(opts, mfa)
   end
 
+  # mix ash.eval 'raise "hello"'
+  #   - throws %RuntimeError
+  #   - script is rescued
+  # mix ash.eval 'raise 1'
+  #   - throws %ArgumentError
+  #   - script is rescued
+  # mix ash.eval 'Process.exit(self(), :stop)'
+  #   - receives an EXIT message
+  # mix ash.eval 'Agent.start_link(fn -> nil end, name: :ash_eval_script)'
+  #   - to test that previous script has died
+
   def capture_eval(pid, code) do
     Process.link(pid)
+    Process.flag(:trap_exit, true)
 
-    try do
-      Code.eval_string(code)
-    rescue
-      e ->
-        # better formatting printing from source
-        IO.inspect(e)
-        IO.inspect(__STACKTRACE__)
-        :rescued
-    after
-      # unlink so that the calling process wont
-      # get killed before getting rpc response
-      Process.unlink(pid)
-    end
+    parent = self()
+
+    child =
+      spawn_link(fn ->
+        try do
+          res = Code.eval_string(code)
+          send(parent, {:result, self(), res})
+        rescue
+          e -> send(parent, {:rescue, self(), e, __STACKTRACE__})
+        end
+
+        # kill process started by the script
+        Process.exit(self(), :done)
+      end)
+
+    # - Exit of the parent must kill the child
+    # - Exit of the child must be catched by the parent
+    # - Linking the child is catcheable but it transitively
+    #   kills the remote process before responding.
+    # - Not linking the child wont't kill processes started
+    #   by the script.
+
+    # The compromise is to log the result locally and just send a
+    # atom response to 'ensure' the response is displayed before
+    # the killing chain reaches the remote process.
+    result =
+      receive do
+        {:result, ^child, res} ->
+          IO.inspect(res)
+          :done
+
+        {:rescue, ^child, e, st} ->
+          # better formatting from source
+          IO.inspect(e)
+          IO.inspect(st)
+          :rescued
+
+        {:EXIT, ^child, reason} ->
+          # better formatting from source
+          IO.inspect(reason)
+          :killed
+
+        msg ->
+          # did the remote died?
+          raise "unexpected #{inspect(msg)}"
+      end
+
+    # unlink so that the calling process wont
+    # get killed before getting rpc response
+    Process.unlink(pid)
+
+    result
   end
 end
